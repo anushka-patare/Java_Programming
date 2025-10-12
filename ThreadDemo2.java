f51b5837e213b93647d4b224d0a4c730a537221c
Anushka Patare <anupatare07@gmail.com>
2025-10-12 21:49:55 +0530

    Add files via upload

---
+++ ThreadDemo2.java
@@ -0,0 +1,15 @@
+class ThreadDemo2 
+{
+     public static void main(String A[])
+     {
+        System.out.println("Inside main method");
+        
+        Thread tobj = Thread.currentThread();
+        String name = tobj.getName();
+
+        System.out.println("Name of current thread is :"+name);
+
+        System.out.println("Priority of current thread is : "+tobj.getPriority());
+
+     }
+}
